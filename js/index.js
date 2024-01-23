12887cf1d4daf89bee9192f37ac692c9d0b2bb48
Matvey Khodosevich <hodos.952795@gmail.com>
2024-01-23 15:31:00 +0300

    logic for project,adaptive version

--- js/index.js
+++ js/index.js
@@ -5,11 +5,20 @@ const closeSidebar = document.getElementById("close-sidebar");
 const deleteContactSidebar = document.getElementById("btn-delete")
 
 const addContactSidebar = document.getElementById("btn-adding")
-const btnSaveGroup = document.getElementById("btn-save")
+const btnSaveGroup = document.getElementById("btn-save-group")
 
 const sidebarNewGroup = document.getElementById("sidebar-new-group");
-let groups = JSON.parse(localStorage.getItem("groups")) || [];
+let data = JSON.parse(localStorage.getItem("data")) || { groups: [], persons: [] };
 
+const groupsData = data.groups || [];
+const membersData = data.persons || [];
+
+const openGroupsSidebar = document.getElementById("choose-group")
+let personsInGroups = JSON.parse(localStorage.getItem("person")) || [];
+
+const cancelNewGroup = document.querySelector(".btn-cancel")
+
+const savePersonInGroup = document.getElementById("btn-save-person")
 
 addContact.addEventListener('click', ()=> {
     openSidebar("Добавление контакта", true)
@@ -21,6 +30,7 @@ groupContact.addEventListener('click', ()=> {
 
 closeSidebar.addEventListener('click', ()=> {
     sidebar.classList.remove("open")
+    document.body.classList.remove("open")
 })
 
 addContactSidebar.addEventListener('click', () => {
@@ -32,19 +42,36 @@ btnSaveGroup.addEventListener('click', () => {
     saveNewGroup()
 })
 
+openGroupsSidebar.addEventListener('click',()=> {
+    openGroupsSidebar.classList.toggle("open")
+})
+
+cancelNewGroup.addEventListener('click',() => {
+    sidebarNewGroup.classList.remove("open")
+})
+
+savePersonInGroup.addEventListener('click', () => {
+    saveNewPerson()
+})
 
 function openSidebar(title,adding) {
     
     let titleElement = sidebar.querySelector('.title-sidebar');
-    
+
+    document.body.classList.add("open")
+
     if(!adding) {
-        document.getElementById("btn-adding").classList.add("display-block")
+        document.getElementById("btn-adding").classList.add("open")
         document.getElementById("content-sidebar-form").classList.remove("open")
         document.getElementById("content-sidebar-groups").classList.add("open")
+        document.getElementById("btn-save-group").classList.add("open")
+        document.getElementById("btn-save-person").classList.remove("open")
     }else {
-        document.getElementById("btn-adding").classList.remove("display-block")
+        document.getElementById("btn-adding").classList.remove("open")
         document.getElementById("content-sidebar-form").classList.add("open")
         document.getElementById("content-sidebar-groups").classList.remove("open")
+        document.getElementById("btn-save-group").classList.remove("open")
+        document.getElementById("btn-save-person").classList.add("open")
     }
     
     titleElement.textContent = title;
@@ -55,6 +82,8 @@ function openSidebar(title,adding) {
 
 function closeSidebarHandler() {
     sidebar.classList.remove('open');
+    document.body.classList.remove("open")
+    sidebarNewGroup.classList.remove("open")
 }
 
 
@@ -81,14 +110,22 @@ function saveNewGroup() {
     if (groupInputValue !== null && groupInputValue.trim() !== "") {
         sidebarNewGroup.classList.remove("open");
 
-        groups.push(groupInputValue.trim());
+        const newGroup = {
+            id: generateUniqueId(),
+            name: groupInputValue,
+        }
+        data.groups.push(newGroup);
         
-        localStorage.setItem("groups", JSON.stringify(groups));
+        localStorage.setItem("data", JSON.stringify(data));
 
         renderAllComponents();
         document.getElementById("groupInput").value = "";
     }
 
+}
+
+function generateUniqueId() {
+    return Date.now().toString(36);
 }
 
 function deleteGroup(event) {
@@ -97,14 +134,12 @@ function deleteGroup(event) {
     const groupName = groupElement.querySelector('.content-sidebar-groups__item-group').textContent.trim();
 
     const groupsContainer = document.getElementById('content-sidebar-groups');
-    groupsContainer.removeChild(groupElement);
 
-    groups = groups.filter(item => item.trim() !== groupName.trim())
-        
-    localStorage.setItem("groups", JSON.stringify(groups));
+    groupsContainer.removeChild(groupElement);
+    data.groups = data.groups.filter(item => item.name !== groupName)
+    localStorage.setItem("data", JSON.stringify(data));
 
     event.stopPropagation()
-    
     renderAllComponents();
 }
 
@@ -115,12 +150,12 @@ function createGroupElement(groupName) {
 
     const groupContent = `
         <div class="content-sidebar-groups__item-group">${groupName}</div>
-        <img id="btn-delete" class="content-sidebar-groups__item-img delete-group" src="./assets/Delete forever.svg" alt="delete">
+        <img id="btn-delete" class="delete-img" src="./assets/Delete forever.svg" alt="delete">
     `;
 
     groupElement.innerHTML = groupContent;
 
-    const deleteGroupButton = groupElement.querySelector('.delete-group');
+    const deleteGroupButton = groupElement.querySelector('.delete-img');
     deleteGroupButton.addEventListener('click', deleteGroup);
 
     return groupElement;
@@ -132,43 +167,238 @@ function renderGroups() {
 
     groupsContainer.innerHTML = '';
 
-    console.log(groups);
-    groups.forEach(groupName => {
-        const groupElement = createGroupElement(groupName);
-        groupsContainer.appendChild(groupElement);
-    });
+
+    if (data.groups && Array.isArray(data.groups)) {
+        data.groups.forEach(group => {
+            const groupElement = createGroupElement(group.name);
+            groupsContainer.appendChild(groupElement);
+        });
+    } else {
+        console.error("Ошибка: data.groups не является массивом или отсутствует.");
+    }
 }
 
+function saveNewPerson() {
+    const nameInput = document.getElementById("content-sidebar-form-person").value
+    const numberInput = document.getElementById("content-sidebar-form-number").value
+    const selectInput = document.getElementById("choose-group").value
 
+    if (!nameInput || !numberInput || !selectInput) {
+        alert("Пожалуйста, заполните все поля.");
+        return;
+    }
 
+    const phoneNumberRegex = /^\+\d{7,}$/;
+    if (!phoneNumberRegex.test(numberInput)) {
+        alert("Пожалуйста, введите корректный номер телефона.");
+        return;
+    }
+
+    if(selectInput < 0) {
+        alert("Пожалуйста, выберите группу.");
+        return;
+    }
+
+    const newPerson = {
+        name: nameInput,
+        phone: numberInput,
+        groupId: selectInput,
+        personId: generateUniqueId()
+    }
+
+    data.persons.push(newPerson)
+
+        localStorage.setItem("data", JSON.stringify(data));
+
+    document.getElementById("content-sidebar-form-person").value = "";
+    document.getElementById("content-sidebar-form-number").value = "";
+    document.getElementById("choose-group").value = -1;
+
+    closeSidebarHandler()
+
+}
 
 function renderListGroups() {
 
     const content = document.getElementById('content-list-items');
 
-    console.log(groups);
-    if (groups && groups.length > 0) {
+    if (data.groups && data.groups .length > 0) {
         let groupsHTML = '';
 
-        for (const key in groups) {
+        for (const key in data.groups) {
             groupsHTML += `
-                <div class="content-list-item">
-                    <h3 class="content-list-item-title">
-                        ${groups[key]}
-                    </h3>
-                    <img src="./assets/Keyboard arrow right.svg" alt="Keyboard arrow right">
+                     <div class="content-list-item" >
+                            <div class="content-list-item-header">
+                                <h3 id="content-list-item-title-${data.groups[key].id}" class="content-list-item-title">
+                                    ${data.groups[key].name}
+                                </h3>
+                                <img class="keyboard-arrow-right" data-id="${data.groups[key].id}" id="keyboard-arrow-right" src="./assets/Keyboard arrow right.svg" alt="Keyboard arrow right">         
+                            </div>
+                              <div class="content-group-list">
+                                <div id="content-group-list-items-${data.groups[key].id}" class="content-group-list-items">
+                                    <div class="content-group-list-item">
+                                        <h4>
+                                            Фамилия Имя Отчество
+                                        </h4>
+                                        <div class="content-group-list-item-right">
+                                            <p >
+                                                +7 (ХХХ) ХХХ - ХХ - ХХ
+                                            </p>
+                                            <div class="content-group-list-item-right-imgs" >
+                                                <img class="change-note" src="./assets/Mode edit.svg" alt="mode-edit">
+                                                <img class="delete-img" src="./assets/Delete forever.svg" alt="delete">
+                                            </div>
+                                        </div>
+                                    
+                                    </div>
+                                    <div class="content-group-list-item">
+                                        <h4>
+                                            Фамилия Имя Отчество
+                                        </h4>
+                                        <div class="content-group-list-item-right">
+                                            <p >
+                                                +7 (ХХХ) ХХХ - ХХ - ХХ
+                                            </p>
+                                            <div class="content-group-list-item-right-imgs" >
+                                                <img class="change-note" src="./assets/Mode edit.svg" alt="mode-edit">
+                                                <img class="delete-img" src="./assets/Delete forever.svg" alt="delete">
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                        </div>`;
+        }
+
+        content.innerHTML = groupsHTML;
+
+        const arrowButtons = document.querySelectorAll('.keyboard-arrow-right');
+        arrowButtons.forEach(button => {
+            button.addEventListener('click', () => {
+                const groupId = button.getAttribute('data-id');
+                const groupContent = document.getElementById(`content-group-list-items-${groupId}`);
+                const groupTitle = document.getElementById(`content-list-item-title-${groupId}`);
+
+                groupContent.classList.toggle('open');
+                button.classList.toggle("open")
+                groupTitle.classList.toggle("open")
+
+                if (groupContent.classList.contains('open')) {
+                    renderGroupMembers(groupId);
+                }
+
+            });
+        });
+
+    } else {
+        content.innerHTML = `<h1 class="title-content">Список контактов пуст</h1>`;
+    }
+}
+
+
+function renderGroupMembers(groupId) {
+    const groupContent = document.getElementById(`content-group-list-items-${groupId}`);
+    groupContent.innerHTML = '';
+
+    data.persons
+        .filter(person => person.groupId === groupId)
+        .forEach(person => {
+            const listItem = document.createElement('div');
+            listItem.classList.add('content-group-list-item');
+            listItem.innerHTML = `
+                <h4>${person.name}</h4>
+                <div class="content-group-list-item-right">
+                    <p>${person.phone}</p>
+                    <div class="content-group-list-item-right-imgs">
+                        <img id="change-note" class="change-note" data-id="${person.personId}" src="./assets/Mode edit.svg" alt="mode-edit">
+                        <img class="delete-img" data-id="${person.personId}" src="./assets/Delete forever.svg" alt="delete">
+                    </div>
                 </div>`;
+            groupContent.appendChild(listItem);
+        });
+
+    const changeNoteButtons = groupContent.querySelectorAll('.change-note');
+    changeNoteButtons.forEach(button => {
+        button.addEventListener('click', () => {
+            const personId = button.getAttribute('data-id');
+            openNoteModal(personId);
+        });
+    });
+
+    const deleteImgButtons = groupContent.querySelectorAll('.delete-img');
+    deleteImgButtons.forEach(button => {
+        button.addEventListener('click', () => {
+            const personId = button.getAttribute('data-id');
+            const person = data.persons.find(person => person.personId === personId);
+            const groupId = person.groupId;
+
+            data.persons = data.persons.filter(person => person.personId !== personId);
+            localStorage.setItem("data", JSON.stringify(data));
+            renderGroupMembers(groupId);
+            const groupTitle = document.getElementById(`content-list-item-title-${groupId}`);
+            groupTitle.classList.remove("open")
+        });
+    });
+}
+
+function openNoteModal(personId) {
+    const noteModal = document.getElementById('noteModal');
+    const closeNoteModal = document.getElementById('closeNoteModal');
+    const noteTextArea = document.getElementById('noteTextArea');
+    const saveNoteButton = document.getElementById('saveNoteButton');
+
+    const person = data.persons.find(person => person.personId === personId);
+
+    noteTextArea.value = person.note || '';
+
+    noteModal.style.display = 'block';
+
+    closeNoteModal.addEventListener('click', () => {
+        noteModal.style.display = 'none';
+    });
+
+    saveNoteButton.addEventListener('click', () => {
+        const newNote = noteTextArea.value;
+
+        const updatedPersons = data.persons.map(person => {
+            if (person.personId === personId) {
+                return { ...person, note: newNote };
+            }
+            return person;
+        });
+
+        data.persons = updatedPersons;
+        localStorage.setItem("data", JSON.stringify(data));
+
+        noteModal.style.display = 'none';
+
+        renderGroupMembers(person.groupId);
+    });
+}
+
+
+function renderGroupsSidebar () {
+    const content = document.getElementById('choose-group');
+
+    let groupsHTML = `<option value="-1">Выберите группу:</option>`;
+    console.log(data.groups );
+    if (data.groups  && data.groups.length > 0) {
+        
+
+        for (const key in data.groups) {
+            groupsHTML += `
+            <option value="${data.groups[key].id}">${data.groups[key].name}</option>`;
         }
 
         content.innerHTML = groupsHTML;
     } else {
-        content.innerHTML = `<h1 class="title-content">Список контактов пуст</h1>`;
+        content.innerHTML = `<h1 class="title-content">Список групп пуст</h1>`;
     }
 }
 
 function renderAllComponents(){
     renderGroups();
     renderListGroups();
+    renderGroupsSidebar();
 }
 
 renderAllComponents()
